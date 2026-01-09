326142113ec751244dc4edd0df28628b51fc0d03
José Briceño <josea2102@gmail.com>
2026-01-08 22:02:17 -0400

    Preparando el terreno para el Gateway (tengo miedo)

---
+++ src/lib/multiplayer/infrastructure/helpers/SocketInterface.ts
@@ -0,0 +1,3 @@
+import { Socket } from "socket.io";
+import { HostUserEvents, PlayerUserEvents, ServerErrorEvents, ServerEvents, ClientEvents } from "./WebSocketEvents.enum";
+import { SessionRoles } from "./SessionRoles.enum";
