de18c432702822841edc3d840a5ebacfd330a441
jorguitoramirez <jorguitoramirez@gmail.com>
2026-01-10 03:14:40 +0000

    Cambios en el c[odigo de media

--- src/lib/media/domain/value-object/MediaId.ts
+++ src/lib/media/domain/value-object/MediaId.ts
@@ -2,9 +2,11 @@
 import { randomUUID } from 'crypto';
 import { DomainException } from '../../../shared/exceptions/domain.exception';
 
-const UUID_V4_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-function isValidUUID(value: string): boolean { return UUID_V4_REGEX.test(value); }
+const UUID_ANY_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
+function isValidUUID(value: string): boolean {
+  return UUID_ANY_REGEX.test(value);
+}
 // --- VOs de Identidad ---
 export class MediaId {
     private constructor(public readonly value: string) { if (!isValidUUID(value)) { throw new DomainException(`MediaId does not have a valid UUID v4 format: ${value}`); } }
